4fa85032820acd4e90bbc2c55c0acdbecf4adeff
FatLi <514340686@qq.com>
2019-07-08 08:17:45 +0800

    today

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 window.app = {
-	
-	serverUrl: 'http://172.16.1.110:8888',
-	 
-	
-	
+
+	serverUrl: 'http://192.168.1.9:8888',
+
+
+
 	imageUrl: 'http://106.12.137.47:8888/group1/',
-	
+
 	/**
 	 * 判断字符串是否为空
 	 * @param {Object} str
@@ -13,21 +13,24 @@ window.app = {
 	 * false：为空
 	 */
 	isNotNull: function(str) {
-		if (str != null &&str != "" && str != undefined) {
+		if (str != null && str != "" && str != undefined) {
 			return true;
-		} 
+		}
 		return false;
 	},
-	
+
 	/**
 	 * 封装消息提示框，默认mui的不支持居中和自定义icon，所以使用h5+
 	 * @param {Object} msg
 	 * @param {Object} type
 	 */
 	showToast: function(msg, type) {
-		plus.nativeUI.toast(msg, {icon: "img/"+ type + ".png", verticalAlign:"center"});
+		plus.nativeUI.toast(msg, {
+			icon: "img/" + type + ".png",
+			verticalAlign: "center"
+		});
 	},
-	
+
 	/**
 	 * 保存用户的全局对象
 	 * @param {Object} user
@@ -36,7 +39,7 @@ window.app = {
 		var userInfoStr = JSON.stringify(user);
 		plus.storage.setItem("userInfo", userInfoStr);
 	},
-	
+
 	/**
 	 * 获取用户的全局对象
 	 */
@@ -44,11 +47,33 @@ window.app = {
 		var userInfoStr = plus.storage.getItem("userInfo");
 		return JSON.parse(userInfoStr);
 	},
-	
-		/**
+
+	/**
 	 * 获取用户的全局对象
 	 */
 	userLoginOut: function() {
 		plus.storage.removeItem("userInfo");
-	}	
+	},
+	/**
+	 * 保存用户的联系人列表
+	 * @param {Object} contactList
+	 */
+	setContactList: function(contactList) {
+		var contactListStr = JSON.stringify(contactList);
+		plus.storage.setItem("contactList", contactListStr);
+	},
+
+	/**
+	 * 获取本地缓存中的联系人列表
+	 */
+	getContactList: function() {
+		var contactListStr = plus.storage.getItem("contactList");
+
+		if (!this.isNotNull(contactListStr)) {
+			return [];
+		}
+		return JSON.parse(contactListStr);
+	}
+
+
 }
